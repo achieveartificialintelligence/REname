d0d5e941b060cf4e62b2499c9408fd5f3775d358
Sunny <57467643+achieveartificialintelligence@users.noreply.github.com>
2020-03-22 01:45:06 +0800

    增加注释

--- REname.py
+++ REname.py
@@ -2,6 +2,8 @@
 
 import re
 import os
+
+# ---------以下为PyQt5自动生成--------
 from PyQt5 import QtCore, QtGui, QtWidgets
 
 class Ui_MainWindow(object):
@@ -78,15 +80,20 @@ class Ui_MainWindow(object):
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
-        self.lineEditLuJing.setText(os.getcwd())
+# ---------以上为PyQt5自动生成--------
+
+        #获取当前路径为默认路径
+        self.lineEditLuJing.setText(os.getcwd()) 
+        #出栈历史记录
         self.listWidgetChaZhao.clicked.connect(lambda index: self.lineEditChaZhao.setText(self.listWidgetChaZhao.takeItem(index.row()).text()))
         self.listWidgetTiHuan.clicked.connect(lambda index: self.lineEditTiHuan.setText(self.listWidgetTiHuan.takeItem(index.row()).text()))
-
+        #执行操作
         self.pushButtonYuLan.clicked.connect(lambda: self.zhixing(0))
         self.pushButtonTiHuan.clicked.connect(lambda: self.zhixing(1))
-
+        #文件默认勾选
         self.radioButtonWenJian.setChecked(1)
 
+    # 默认生成
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "REname"))
@@ -99,23 +106,30 @@ class Ui_MainWindow(object):
         self.pushButtonYuLan.setText(_translate("MainWindow", "预览"))
         self.pushButtonTiHuan.setText(_translate("MainWindow", "替换"))
 
+    # 核心函数
     def zhixing(self, status):
         os.system("cls")
+        #奇技，实现do-while，为真说明可能重命名未能全部执行
         loopStatus = True
         while loopStatus:
             loopStatus = False
-
+            #路径
             path = self.lineEditLuJing.text()
+            #遍历所有文件和子目录
             for root, dir, file in os.walk(path):
                 for name in file:
-                    #print("FILE"+os.path.join(root,name))
+                    # print("FILE"+os.path.join(root,name))
+                    #正则
                     res=re.sub(re.compile(self.lineEditChaZhao.text()),self.lineEditTiHuan.text(),name)
+                    #判断文件
                     if name!=res and (self.radioButtonWenJian.isChecked() or self.radioButtonQuanBu.isChecked()) :
                         print(os.path.join(root,name))
+                        #实现↓的位置
                         print(len(root.encode('gbk'))*" "+((name.find(self.lineEditChaZhao.text())+1)*" ")+"↓")
                         print(os.path.join(root,res)+"\n\n")
                         
                         if status:
+                            #异常处理，bug很多，放弃
                             try:
                                 os.rename(os.path.join(root,name),os.path.join(root,res))
                                 loopStatus = True
@@ -123,6 +137,7 @@ class Ui_MainWindow(object):
                             except:
                                 self.statusbar.showMessage("文件重命名存在错误，请检查后重试",0)
                                 break
+                #同上
                 for name in dir:
                     #print("DIR"+os.path.join(root,name))
                     res=re.sub(re.compile(self.lineEditChaZhao.text()),self.lineEditTiHuan.text(),name)
@@ -139,12 +154,12 @@ class Ui_MainWindow(object):
                             except:
                                 self.statusbar.showMessage("目录重命名存在错误，请检查后重试",0)
                                 break
-        
+        #排除重复
         if not self.listWidgetChaZhao.findItems(self.lineEditChaZhao.text(), QtCore.Qt.MatchFlag.MatchExactly):
             self.listWidgetChaZhao.addItem(self.lineEditChaZhao.text())
         if not self.listWidgetTiHuan.findItems(self.lineEditTiHuan.text(), QtCore.Qt.MatchFlag.MatchExactly):
             self.listWidgetTiHuan.addItem(self.lineEditTiHuan.text())
-
+#---以下为自动生成----
 if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
